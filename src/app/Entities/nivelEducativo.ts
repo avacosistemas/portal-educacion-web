4375e144598f91b86eef424369b134f7d26be01e
Alberto Gonzalez <alosgonzalez@gmail.com>
2020-07-07 23:37:44 -0300

    background

--- src/app/Entities/nivelEducativo.ts
+++ src/app/Entities/nivelEducativo.ts
@@ -1,7 +1,14 @@
-export interface NivelEducativo
+import {Materia} from "./materia";
+
+export class NivelEducativo
 {
 	image: string;
 	nombre: string;
 	subtitulo: string;
 	descripcion: string;
+	materias?: Materia[];
+	constructor()
+	{
+		this.materias = [];
+	}
 }
