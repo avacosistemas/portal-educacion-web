78a9921f4bd1c63aaefc7812e5875367f50875ed
Victor <victor.cabas@gmail.com>
2020-09-28 09:31:51 -0300

    nuevos environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // import { PREFIX_DOMAIN_API } from "./environment";
 
-export const PREFIX_DOMAIN_API =  'http://localhost:8080/';
+export const PREFIX_DOMAIN_API =  'https://api.teachonline.com.ar/';
 
 export const environment = {
   production: true,
@@ -8,7 +8,13 @@ export const environment = {
   apiServiceAuth: PREFIX_DOMAIN_API + 'ws-rest-authentication-cliente/',
   apiService: PREFIX_DOMAIN_API + 'ws-rest-educacion/',
   apiServiceCliente: PREFIX_DOMAIN_API + 'ws-rest-educacion-cliente/',
-  recaptchaKey: '6Ldqw74ZAAAAAPrTwwBfaQcsehEPggd0QpJMh6dd',
+
+  //publico de localhost
+  //recaptchaKey: '6Ldqw74ZAAAAAPrTwwBfaQcsehEPggd0QpJMh6dd',
+  //publico de www.teachonline.com.ar
+  //recaptchaKey: '6LdpJL0ZAAAAAO_vludMBF1k-27PZqUwcWe8atI4',
+  //publico de portal.teachonline.com.ar
+  recaptchaKey: '6Lc5OccZAAAAAB3CDi71LUrchfMZktgohQ43Ick3',
 
   localAuth: false,
   security: true,
@@ -16,8 +22,8 @@ export const environment = {
   dummyServices: false,
 
   /** AUTHENTICATION */
-  AUTHENTICATION_URL:  PREFIX_DOMAIN_API + '/ws-rest-authentication/auth',
-  AUTHENTICATION_REFRESH_TOKEN_URL:  PREFIX_DOMAIN_API + '/ws-rest-authentication/refresh',
+  AUTHENTICATION_URL:  PREFIX_DOMAIN_API + 'ws-rest-authentication/auth',
+  AUTHENTICATION_REFRESH_TOKEN_URL:  PREFIX_DOMAIN_API + 'ws-rest-authentication/refresh',
 
   /* Config General URl*/
   URL_ROOT: '',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,30 +3,32 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-export const PREFIX_DOMAIN_API =  'http://ec2-18-215-75-115.compute-1.amazonaws.com:8080/';
+export const PREFIX_DOMAIN_API =  'http://localhost:8080/';
+//export const PREFIX_DOMAIN_API =  'https://api.teachonline.com.ar/';
+//export const PREFIX_DOMAIN_API =  'http://ec2-18-215-75-115.compute-1.amazonaws.com:8080/';
 
 export const environment = {
 
-    apiServiceAuth: PREFIX_DOMAIN_API + 'ws-rest-authentication-cliente/',
-    apiService: PREFIX_DOMAIN_API + 'ws-rest-educacion/',
-    apiServiceCliente: PREFIX_DOMAIN_API + 'ws-rest-educacion-cliente/',
-    recaptchaKey: '6Ldqw74ZAAAAAPrTwwBfaQcsehEPggd0QpJMh6dd',
+  apiServiceAuth: PREFIX_DOMAIN_API + 'ws-rest-authentication-cliente/',
+  apiService: PREFIX_DOMAIN_API + 'ws-rest-educacion/',
+  apiServiceCliente: PREFIX_DOMAIN_API + 'ws-rest-educacion-cliente/',
+  recaptchaKey: '6Ldqw74ZAAAAAPrTwwBfaQcsehEPggd0QpJMh6dd',
 
-    localAuth: false,
-    security: true,
-    production: false,
-    hmr: false,
-    dummyServices: false,
+  localAuth: false,
+  security: true,
+  production: false,
+  hmr       : false,
+  dummyServices: false,
 
-    /** AUTHENTICATION */
-    AUTHENTICATION_URL:  PREFIX_DOMAIN_API + '/ws-rest-authentication/auth',
-    AUTHENTICATION_REFRESH_TOKEN_URL:  PREFIX_DOMAIN_API + '/ws-rest-authentication/refresh',
+  /** AUTHENTICATION */
+  AUTHENTICATION_URL:  PREFIX_DOMAIN_API + 'ws-rest-authentication/auth',
+  AUTHENTICATION_REFRESH_TOKEN_URL:  PREFIX_DOMAIN_API + 'ws-rest-authentication/refresh',
 
-    /* Config General URl*/
-    URL_ROOT: '',
-    URL_LOGIN: '/auth/login',
+  /* Config General URl*/
+  URL_ROOT: '',
+  URL_LOGIN: '/auth/login',
 
-    URL_LOGOUT_API: PREFIX_DOMAIN_API + 'user/logout',
+  URL_LOGOUT_API: PREFIX_DOMAIN_API + 'user/logout',
 
 };
 
